eafec9a5cd1f3c05d9056a41d5f83c31fac416ad
Shrivastavadev <dev2shrivastava2@gmail.com>
2024-07-25 20:38:33 +0530

    added some functionalities

---
+++ youTube.js
@@ -0,0 +1,19 @@
+function openVideo(videoLink){
+    window.open(videoLink, "_blank");
+}
+
+function prank(){
+    window.open("prank.html", "_blank")
+}
+
+function notAvailable(){
+    alert("Function Not Available")
+}
+
+function voiceSearch(){
+    alert("Damn it! use ur goddamn fingers")
+}
+
+function ytsearch(){
+    
+}
